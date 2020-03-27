0a89981a17618224ddf020c4d196c754f9e1c345
Kill Toxic <913087364@qq.com>
2020-03-27 15:05:25 +0800

    改

--- edgeExtraction/edgeExtraction.cpp
+++ edgeExtraction/edgeExtraction.cpp
@@ -12,12 +12,17 @@ int main()
 		cv::Mat srcMat;
 		cv::Mat dxMat;
 		cv::Mat dyMat;
+		cv::Mat abs_dxMat;
+		cv::Mat abs_dyMat;
 		cap >> srcMat;//捕捉当前帧
 		Sobel(srcMat, dxMat, CV_16SC1, 1, 0, 3);//用Sobel算子提取x方向上边缘
+		convertScaleAbs(dxMat, abs_dxMat);
 		Sobel(srcMat, dyMat, CV_16SC1, 0, 1, 3);//用Sobel算子提取y方向上边缘
+		convertScaleAbs(dyMat, abs_dyMat);
 		cv::imshow("frame", srcMat);//显示原图像
-		cv::imshow("x", dxMat);//显示提取x方向上边缘的图像
-		cv::imshow("y", dyMat);//显示提取y方向上边缘的图像
+		cv::imshow("x", abs_dxMat);//显示提取x方向上边缘的图像
+		cv::imshow("y", abs_dyMat);//显示提取y方向上边缘的图像
+		convertScaleAbs;
 		waitKey(30);
 	}
 }
